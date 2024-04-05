6b290724cfc0c29d5ead6ec3bacb3b9b87f9a23b
Leonardo Tozzi <leo.tozzi88@gmail.com>
2024-04-05 12:49:33 -0700

    Revision 2

--- analyses_competing_features/analysis_rsonly/02_multiple_imputation.R
+++ analyses_competing_features/analysis_rsonly/02_multiple_imputation.R
@@ -1,11 +1,13 @@
+#### Libraries, imports and global variables ####
+
 library(miceRanger)
 library(VIM)
-
 set.seed(12345)
-
 setwd('/Users/ltozzi/Dropbox (PanLab)/cluster paper/')
-
 data=read.csv('appeal/analyses_competing_features/analysis_rsonly/dataset_merged_qc.csv')
+
+#### Imputation ####
+
 img_vars=c('ic.791103_Left_AG.to.752691_Left_amPFC', 'ic.752691_Left_amPFC.to.414207_Right_AG', 'ic.752691_Left_amPFC.to.073720_Medial_PCC', 'ic.791103_Left_AG.to.073720_Medial_PCC', 'ic.414207_Right_AG.to.073720_Medial_PCC', 'ic.477522_Left_antInsula.to.447647_Left_Amygdala', 'ic.954861_Right_antInsula.to.450767_Right_Amygdala', 'ic.477522_Left_antInsula.to.954861_Right_antInsula', 'ic.532911_Left_lPFC.to.156999_Left_msPFC', 'ic.275836_Right_lPFC.to.156999_Left_msPFC', 'ic.125909_Left_aIPL.to.532911_Left_lPFC', 'ic.525931_Right_aIPL.to.275836_Right_lPFC', 'ic.125909_Left_aIPL.to.831650_Left_precuneus', 'ic.525931_Right_aIPL.to.216192_Right_precuneus')
 
 data_impute=data
